265334ce728a3cb0882816bdf60e59c2ff28dae7
jhallard <jhallard@ucsc.edu>
2015-03-28 02:19:00 -0700

    More work on the C++ RK4 implementation, mostly planning and code-scaffolding at this point

--- cpp_blog/runge_kutta.cpp
+++ cpp_blog/runge_kutta.cpp
@@ -3,6 +3,12 @@
 // the given system. The system trajectory will be stored internally and can be queried over a discretization of points (most likely input as
 // a vector). So we need a RK4 class.
 
+class RK4_Equations;
+
+// @class - RK4
+// @info  - The purpose of this class is to take in a given set of equations (an Rk4_Equations class), and solve them using the RK4 method over
+//          a user-specified discretization. The goal is to allow it to work with an arbitrary number of equations by letting the RK4_Equations 
+//          class be derived from whenever a new set of equations that need to be solved comes up.
 class RK4 {
 
 private:
@@ -15,8 +21,22 @@ private:
 
     double t0, tf;
 
+    RK4_Equations * function_handler;
+
 
 public:
 
     RK4(uint num_equations, double disc, tf);
-}
+};
+
+
+// @class - RK4_Equations
+// @info  - This is to serve as the base class for this hierarchy. All of these classes are meant to be wrappers around sets of equations
+//          that are passed into the RK4 class to be solved.
+class RK4_Equations {
+
+private:
+
+public:
+
+};
