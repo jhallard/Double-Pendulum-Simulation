6cb2457a34ff51fb8e750f5f818ccf5b3341473e
jhallard <jhallard@ucsc.edu>
2015-04-01 15:22:53 -0700

    Starting on the simulation code for the cpp project.

--- cpp/RK4_Test.cpp
+++ cpp/RK4_Test.cpp
@@ -4,10 +4,10 @@
 int main() {
 
     DoublePendEquations dp(9.8, 2.0, 1.0, 2.0, 1.0);
-    FunctionWrapperRK4 * base_ptr = &dp;
+    // FunctionWrapperRK4 * base_ptr = &dp;
     std::vector<double> in = {0.0, -0.5, 1.3, 0.4};
 
-    RK4 rk4(base_ptr);
+    RK4 rk4(&dp);
 
     rk4.solve(0.001, 10, in);
 
