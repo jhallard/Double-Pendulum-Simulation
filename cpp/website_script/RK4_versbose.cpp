3d8b54d7931b9f8473d75091e31d6027cf25363d
jhallard <jhallard@ucsc.edu>
2015-04-04 12:38:11 -0700

    Changed the input arguments in the url to include all initial conditions, system constans, and simulation parameters. I took the output out of the solve function and placed it after the query function.

--- cpp/website_script/RK4_versbose.cpp
+++ cpp/website_script/RK4_versbose.cpp
@@ -80,11 +80,13 @@ bool RK4::solve(double disc, double tf, std::vector<double> ic) {
 
         // start = std::chrono::high_resolution_clock::now(); // #remove
 
-        for(int j = 0; j < states.size(); j++) {
-            // _solutions[j][i] = states[j];
-            printf("%5.3f ", states[j]);
-        }
-        printf("\n");
+        // if(i % 10 == 1) {
+            for(int j = 0; j < states.size(); j++) {
+                _solutions[j][i] = states[j];
+                // printf("%5.3f ", states[j]);
+            }
+            // printf("\n");
+        // }
 
         // # remove next three
         // elapsed = std::chrono::high_resolution_clock::now() - start;   
--- cpp/website_script/get_values.cpp
+++ cpp/website_script/get_values.cpp
@@ -24,10 +24,26 @@ int main ()
 
    double tf = 2.5;
    double disc = 0.00001;
+   double step = 0.01666;
 
+   std::vector<double> ic = {9.8, 2.1, 1.2, 1.5, 0.9, 0.5, -0.5, 1.3, 1.2};
+   std::vector<double> ic_str  = {"grav", "len1", "mass1", "len2", "mass2", "theta1", "theta2", "dtheta1", "dtheta2"};
+
+   form_iterator fi; 
+   for(int i = 0; i < ic.size(); i++) {
+      fi = formData.getElement(ic_str[i]);  
+      if( !fi->isEmpty() && fi != (*formData).end()) {  
+         ic[i] = atof((**fi).c_str());  
+      }
+   }
    form_iterator fi = formData.getElement("disc");  
    if( !fi->isEmpty() && fi != (*formData).end()) {  
       disc = atof((**fi).c_str());  
+   }
+
+   form_iterator fi = formData.getElement("step");  
+   if( !fi->isEmpty() && fi != (*formData).end()) {  
+      step = atof((**fi).c_str());  
    }
 
    fi = formData.getElement("tf");  
@@ -37,21 +53,21 @@ int main ()
    
    cout << "Content-type:text/plain\r\n\r\n";
 
-    DoublePendEquations dp(9.8, 2.0, 1.0, 2.0, 1.0);
+    DoublePendEquations dp(ic[0], ic[1], ic[2], ic[3], ic[4]);
     // FunctionWrapperRK4 * base_ptr = &dp;
-    std::vector<double> in = {0.0, -0.5, 1.3, 0.4};
+    std::vector<double> in = {ic[5], -ic[6], ic[7], ic[8]};
 
     RK4 rk4(&dp);
 
     rk4.solve(disc, tf, in);
 
-    auto ret = rk4.query(0, tf, disc*10.0);
+    auto ret = rk4.query(0, tf, step);
 
     // int it = 1; 
     // printf("theta1 Theta2 dTheta1 dTheta2\n");
     for(int i = 0; i < ret[0].size(); i++) {
         for(int j = 0; j < ret.size(); j++) {
-          printf("%4.2f ", ret[j][i]);
+          printf("%5.3f ", ret[j][i]);
         }
         printf("\n");
     }
