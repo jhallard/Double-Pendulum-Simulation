ce2fc7f49804977b782f80d1bcd7a16eb857c7dc
jhallard <jhallard@ucsc.edu>
2015-04-04 03:36:44 -0700

    Slight changes to testing files for cgi output

--- cpp/print_values.cpp
+++ cpp/print_values.cpp
@@ -34,6 +34,4 @@ int main(int argc, char ** argv) {
     }
 
     return 0;
-
-
 }
---
+++ cpp/website_script/get_values.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>  
+#include <string>  
+#include <stdio.h>  
+#include <stdlib.h> 
+#include <iostream>
+
+#include <cgicc/CgiDefs.h> 
+#include <cgicc/Cgicc.h> 
+#include <cgicc/HTTPHTMLHeader.h> 
+#include <cgicc/HTMLClasses.h> 
+
+#include "~/Double-Pendulum-Simulation/cpp/RK4.h"
+#include "~/Double-Pendulum-Simulation/cpp/DoublePendEquations.h"
+
+
+using namespace std;
+using namespace cgicc;
+
+
+int main ()
+{
+   Cgicc formData;
+
+   double tf = 2.5;
+   double disc = 0.00001;
+
+   form_iterator fi = formData.getElement("disc");  
+   if( !fi->isEmpty() && fi != (*formData).end()) {  
+      disc = atof(**fi);  
+   }
+
+   fi = formData.getElement("tf");  
+   if( !fi->isEmpty() &&fi != (*formData).end()) {  
+      tf = atof(**fi);  
+   }
+   
+   cout << "Content-type:text/plain\r\n\r\n";
+
+    DoublePendEquations dp(9.8, 2.0, 1.0, 2.0, 1.0);
+    // FunctionWrapperRK4 * base_ptr = &dp;
+    std::vector<double> in = {0.0, -0.5, 1.3, 0.4};
+
+    RK4 rk4(&dp);
+
+    rk4.solve(disc, tf, in);
+
+    auto ret = rk4.query(0, tf, 0.001);
+
+    // int it = 1; 
+    // printf("theta1 Theta2 dTheta1 dTheta2\n");
+    for(int i = 0; i < ret[0].size(); i++) {
+        for(int j = 0; j < ret.size(); j++) {
+          printf("%4.2f ", ret[j][i]);
+        }
+        printf("\n");
+    }
+   
+   return 0;
+}
